63e14bc337c387b9e3ef81ee1c8ab603f711a947
Aditya Poddar <poddar.aditya2014@gmail.com>
2022-10-04 15:31:52 +0530

    initial commit

---
+++ Tower_Of_Hanoi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ void toh(int n,char src,char dest,char aux)
+ {
+     if(n==1)
+     {
+         printf("\n Move disc %d from %c to %c",n,src,dest);
+     }
+     else
+     {
+         toh(n-1,src,aux,dest);
+         printf("\n Move disc %d from %c to %c",n,src,dest);
+         toh(n-1,aux,dest,src);
+     }
+ }
+ void main()
+ {
+     toh(3,'A','B','C');
+ }
